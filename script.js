297ce6312cdb3abd664e5b1ffce8b27653edb226
Brandon Romaine <brandonmromaine@gmail.com.com>
2022-08-22 10:57:49 -0400

    JS: Test XMLHttprequest, JSON: Test dummy JSON data

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const getWeather = function(){
-    return new Promise((resolve,reject)=>{
-    fetch('https://jsonplaceholder.typicode.com/todos/1');
-    resolve('Success');
-    reject('error');
-})
+let endPoint = 'http://jsonplaceholder.typicode.com/users';
+
+const getData = (endPoint)=>{
+const request = new XMLHttpRequest();
+request.addEventListener('readystatechange',()=>{
+if (request.readyState === 4 && request.status === 200){
+    console.log(request.responseText);
+} else if(request.readyState === 4){
+    console.log('error, could not retieve data');
+}
+});
+
+
+request.open('GET', endPoint)
+request.send();
 }
 
 
-getWeather.then((resolve)=>{
-    console.log('success');
-}).catch((reject)=>{
-    console.log('error');
-});
+getData(endPoint);
+
