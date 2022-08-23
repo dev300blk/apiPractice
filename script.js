69b52394366b0c93f1af1c85a314c97600f0835b
Brandon Romaine <brandonmromaine@gmail.com.com>
2022-08-23 08:07:00 -0400

    JS: api call achieved with promise

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
-const test = 0;
-
-const getData = (callBack)=>{
-const request = new XMLHttpRequest();
-request.addEventListener('readystatechange',()=>{
-if (request.readyState === 4 && request.status === 200){
-   callBack(undefined, request.responseText);
-} else if(request.readyState === 4){
-    callBack('could not fetch data', undefined);
-}
-});
 
 
-request.open('GET','test.json');
-request.send();
-}
+// const getData = (callBack)=>{
+//     const request = new XMLHttpRequest();
+//     request.addEventListener('readystatechange',()=>{
+//     if (request.readyState === 4 && request.status === 200){
+//        callBack(undefined, request.responseText);
+//     } else if(request.readyState === 4){
+//         callBack('could not fetch data', undefined);
+//     }
+//     });
+    
+    
+//     request.open('GET','test.json');
+//     request.send();
+//     }
+    
+    
+//     getData((err,data)=>{
+//         if(err){
+//             console.log(err);
+//         } else if(data){
+//             console.log(data);
+//             const obj = JSON.parse(data);
+//             console.log(obj);
+//         }
+//     });
 
-
-getData((err,data)=>{
-    if(err){
-        console.log(err);
-    } else if(data){
-        console.log(data);
-        const obj = JSON.parse(data);
-        console.log(obj);
+const getData = (rescource)=>{
+    return new Promise((resolve,reject)=>{
+        const request = new XMLHttpRequest();
+            request.addEventListener('readystatechange',()=>{
+            if (request.readyState === 4 && request.status === 200){
+                const data = JSON.parse(request.responseText)
+               resolve(data);
+            } else if(request.readyState === 4){
+                reject('error');
+            }
+            });
+            
+            
+            request.open('GET',rescource);
+            request.send();
+        //     }
     }
-});
+)};
 
+getData('test.json').then((data)=>{
+    console.log(data);
+}).catch((err)=>{
+    console.log(err);
+})
